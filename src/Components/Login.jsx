323e73f5859acc843619854657af5de908a7ea4a
Dhruv <ruvycodes@gmail.com>
2024-03-04 21:57:36 +0530

    Form Validation

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,25 +1,38 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { useValidateAuth } from '../Utils/useValidateAuth';
 
 const Login = () => {
 
   const [haveAccount, setHaveAccount] = useState(false)
+  const [authMessage, setAuthMessage] = useState("")
+  const email = useRef()
+  const password = useRef()
+
   const handleSignUp = () => {
-    setHaveAccount(!haveAccount)
+    setHaveAccount(!haveAccount) //toggle
+  }
+
+  const handleSubmit = () => {
+    let message = useValidateAuth(email.current.value, password.current.value) //pass the value to the custom hook to validate
+    setAuthMessage(message)
   }
 
   return (
     <div className='w-3/12 bg-slate-500 h-[32rem] mx-auto mt-20'>
       <div className='flex justify-center items-center flex-col'>
         <h1 className='text-2xl mt-[4rem] mb-10'>{!haveAccount ? "Sign In" : "Sign Up"}</h1>
-        <form action="">
+        <form onSubmit={(e) => e.preventDefault()} action=""> {/*this is to prevent the default behaviour of submitting the form*/}
           <div className='flex flex-col items-center'>
             {haveAccount && <input className='w-64 py-2 px-3 rounded-sm mb-5' placeholder='Enter Name' type="text" name="" id="" />}
-            <input className='w-64 py-2 px-3 rounded-sm mb-5' placeholder='Enter Email Address' type="text" name="" id="" />
-            <input className='w-64 py-2 px-3 rounded-sm mb-5' placeholder='Enter Password' type="text" name="" id="" />
+            <input className='w-64 py-2 px-3 rounded-sm mb-5' ref={email} placeholder='Enter Email Address' type="text" name="" id="" />
+            <input className='w-64 py-2 px-3 rounded-sm mb-5' ref={password} placeholder='Enter Password' type="text" name="" id="" />
             {haveAccount && <input className='w-64 py-2 px-3 rounded-sm mb-5' placeholder='Confirm Password' type="text" name="" id="" />}
           </div>
+          <div className='py-1 mb-2 px-1'>
+            <span className='text-red-500'>{authMessage}</span>
+          </div>
           <div className='mb-6 mt-1'>
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded'>
+            <button onClick={handleSubmit} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded'>
               Submit
             </button>
           </div>
---
+++ src/Utils/useValidateAuth.jsx
@@ -0,0 +1,18 @@
+export const useValidateAuth = (email, password) => {
+    // we will simply use regex to validate the mail and pass , we can also modify this regex according to our need
+    let validEmail = /^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,})$/.test(email);
+    let validPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
+    console.log(validEmail, validPassword);
+
+    if (!validEmail) {
+        return "Email is not valid"
+    }
+
+    if (!validPassword) {
+        return "Password is not valid"
+    }
+
+    return null
+
+
+}
